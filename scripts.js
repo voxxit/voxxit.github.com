ad3e94efb52eee6112c423483ee6913c53d2f28e
Josh Delsman <jdelsman@voxxit.com>
2009-12-18 21:17:15 +0000

    Added Google Chrome Frame

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function write_email(){ 
+  document.write([">", "a", "/", "<", "m", "o", "c", ".", "t", "i", "x", "x", "o", "v", "@", "n", "a", "m", "s", "l", "e", "d", "j", ">", "\"", "m", "o", "c", ".", "t", "i", "x", "x", "o", "v", "@", "n", "a", "m", "s", "l", "e", "d", "j", ":", "o", "t", "l", "i", "a", "m", "\"", "=", "f", "e", "r", "h", " ", "a", "<"].reverse().join('')); 
+}
+
+var _gaq = _gaq || [];
+
+_gaq.push(['_setAccount', 'UA-489504-10']);
+_gaq.push(['_trackPageview']);
+
+(function(){
+  var ga = document.createElement('script');
+  
+  ga.type = 'text/javascript'; 
+  ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
+})();
